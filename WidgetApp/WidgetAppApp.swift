6d616de31ec08f13118b8595340feedeb09bc943
jantiks <58886410+jantiks@users.noreply.github.com>
2021-02-08 08:35:52 +0400

    Initial Commit

---
+++ WidgetApp/WidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetAppApp.swift
+//  WidgetApp
+//
+//  Created by Tigran on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct WidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
